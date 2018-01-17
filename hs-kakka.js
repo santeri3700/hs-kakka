0ce23ff9740d6aa0aaf54188b7af380208cdb60f
santeri3700 <santeri.pikarinen@gmail.com>
2018-01-17 21:16:45 +0200

    Add files via upload

---
+++ hs-kakka.js
@@ -0,0 +1,4 @@
+var hskakkaStyle = document.createElement("style");
+hskakkaStyle.innerText = '.premium-subscribers-note:before { content: "\\01F4A9"; } .article-paywall .hardwall-symbol:before, .facsimile-paywall .hardwall-symbol:before { content: "\\01F4A9"; line-height: unset; }';
+hskakkaStyle.setAttribute('type', 'text/css');
+document.head.appendChild(hskakkaStyle);
